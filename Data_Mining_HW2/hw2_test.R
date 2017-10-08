dfc427add7529e8e79d2ef5afdcbc1a40ed9db60
Arman Zakaryan <armzak111@gmail.com>
2017-10-08 17:42:45 +0400

    hw2 first draft

--- Data_Mining_HW2/hw2_test.R
+++ Data_Mining_HW2/hw2_test.R
@@ -2,7 +2,7 @@
 load(file = 'UN_Armenia.rda')
 str(armdata)
 summary(armdata)
-View(head(armdata, n=100))
+#View(head(armdata, n=100))
 armdata$amend <- NULL
 armdata$para <- NULL
 
@@ -13,21 +13,20 @@ summary(armdata$vote)
 
 #P.3
 voted <- armdata$vote!='abstain' 
-regr_data <- lm(formula =  armdata$importantvote ~ voted)
-summary(regr_data)
+regr_data <- t.test(armdata$importantvote ~ voted)
+regr_data
 
 #null hyp - voting doesn't depend on importance
 #probably reject because of 0 p value, to be edited
 
 #P.4
 class(armdata$armpres)
-regr_data2 <- lm(formula = armdata$armpres ~ armdata$vote)
-summary(regr_data2)
+tbl <- table(armdata$armpres, armdata$vote)
+chisq.test(tbl)
 
-table(armdata$vote)
-?lm
-
-#Errors, definitely rewrite
+#null hyp - voting pattern is independent from president
+#reject, as p value is 0. 
+#to be edited
 
 #P.5
 subs <- armdata[armdata$country=='Armenia',]
@@ -37,5 +36,44 @@ table(subs[,c('issue', 'vote')])
 #P.6
 library(HistData)
 df <- GaltonFamilies
-regr_data3 <- lm(df$childHeight~df$gender)
-summary(regr_data3)
+regr_data3 <- t.test(childHeight~gender, data=df)
+regr_data3
+
+#P.7
+autompg <- read.csv('auto-mpg.csv')
+str(autompg)
+autompg$horsepower <- as.numeric(autompg$horsepower)
+autompg$origin <- as.factor(autompg$origin)
+
+#P.8
+set.seed(1)
+train_ind <- sample(nrow(autompg),277)
+am_train <- autompg[train_ind,]
+am_test <- autompg[-train_ind,]
+
+#P.9  
+cor(autompg[,1:7])
+#first: weight, second: displacement
+
+#P.10
+regr_data_mw <- lm(formula = mpg~weight, data=autompg)
+regr_data_mw$coefficients
+summary(regr_data_mw)$adj.r.squared
+regr_model <- lm(formula = mpg~weight, data=am_train)
+am_pred <- predict(regr_model, newdata=am_test)
+sqrt(mean((am_pred-am_test$mpg)^2))
+
+#P.11
+regr_data_md <- lm(formula = mpg~displacement, data=autompg)
+regr_data_md$coefficients
+summary(regr_data_md)$adj.r.squared
+regr_model2 <- lm(formula = mpg~displacement, data=am_train)
+am_pred2 <- predict(regr_model2, newdata=am_test)
+sqrt(mean((am_pred2-am_test$mpg)^2))
+
+#P.12
+regr_model <- lm(formula = mpg~weight+model.year, data=am_train)
+am_test_pred <- predict(regr_model, newdata=am_test)
+sqrt(mean((am_test_pred-am_test$mpg)^2))
+am_train_pred <- predict(regr_model, newdata = am_train)
+sqrt(mean((am_train_pred-am_train$mpg)^2))
