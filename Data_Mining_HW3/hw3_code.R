e0529f1050e3d9e02d30af0bef5c537af47ba93e
Arman Zakaryan <armzak111@gmail.com>
2017-10-21 23:39:44 +0400

    hw3 probably final

--- Data_Mining_HW3/hw3_code.R
+++ Data_Mining_HW3/hw3_code.R
@@ -15,27 +15,33 @@ dsmall_dist <- dist(dsmall, method = 'euclidian')
 dsmall_dist
 
 #Pr.4
+set.seed(1)
 hc <- hclust(dsmall_dist, method = 'complete')
-#TBE
+#This method defines the distance between clusters as  the distance between the two furthest apart members of those clusters
 
 #Pr.5
 plot(hc)
 rect.hclust(hc, k = 3)
-#TBE
+#Its distance was greater from the already formed clusters, so it got left out.
 
 #Pr.6
 hc$merge
-#TBE
+#negative signs means a singleton, non negative sign number means the cluster formed at row [number] and in each row it
+#shows that in that step the two entries have been merged
 
 #Pr.7
-dsmall[,2:8] <- scale(dsmall[,2:8], center=T, scale=T)
-#TBE
+dsmall[,3:8] <- scale(dsmall[,3:8], center=T, scale=T)
+#center=T subtracts column means from corresponding columns of data
+#scale=T divides each column of data by its standard deviation
 
 #Pr.8
-hc <- hclust(dist(dsmall), method = 'complete')
-plot(hc)
-rect.hclust(hc, k = 3)
-#TBE
+set.seed(1)
+hc1 <- hclust(dist(dsmall), method = 'complete')
+plot(hc1)
+rect.hclust(hc1, k = 3)
+#changed, significant, as we can see many things vars their clusters
+#reason is that with normalized data we don't have a scale conflict, so distances are 
+#different from initial data, so the dendrogram looks different
 
 #Pr.9
 wdi <- read.csv('WDI indicators.csv')
@@ -44,19 +50,40 @@ wdi_scaled <- as.data.frame(scale(wdi[,3:10], center=T, scale=T))
 
 #Pr.10
 tots={}
-for(i in c(1:15)){
+for(i in c(1:10)){
   set.seed(1)
   tots[i]=kmeans(wdi_scaled, i)$tot.withinss
 }
-plot(1:15,tots, type='b', pch=19, frame=FALSE)
-opt_clust <- 8
-#probably 11?
+plot(1:10,tots, type='b', pch=19, frame=FALSE)
+opt_clust <- 5
+#by elbow method it's 5
 
 #Pr.11
+set.seed(1)
 km <- kmeans(wdi_scaled, opt_clust)
 km$cluster
 wdi$cluster <- as.factor(km$cluster)
 
+
 #Pr.12
-aggregate(.~cluster, data=wdi, FUN = 'mean')
 View(wdi)
+aggregate(.~cluster, data=wdi, FUN = 'mean')
+#economically developed countries, upper end third world countries, lower end third world countries, high population
+#first world countries
+
+#Pr.13
+#by looking at the means we can see that the above labels for clusters are reasonable
+
+#Pr.14
+View(wdi)
+agg <- aggregate(Country~cluster, data=wdi, print)
+#generally yes, surrounded with other post soviet countries
+
+#Pr.15
+library(FactoMineR)
+ca <- PCA(wdi[3:10], ncp=8, graph = F)
+plot(ca,choix="var", cex=0.5,invisible="quanti.sup")
+summary(ca)
+#dim1 explains 38.87% of variance, dim2 explains 19.72%.
+#dim1 strongly correlates with neimpgnfszs and neexpgnfszs
+#dim2 strongly correlates with nygdppcapcd and spdyn...
